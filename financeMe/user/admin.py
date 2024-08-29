244ca05b2a5a5968f917a1d7c0d8d0820dbd59ca
Prithvi Paturi <87906760+prithvipat@users.noreply.github.com>
2024-08-28 21:16:28 -0400

    Update admin.py

--- financeMe/user/admin.py
+++ financeMe/user/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Profile, Transactions
-# Register your models here.
+from .models import Subscriptions, Transactions, Profile, Budget
 
+# Register your models here.
 admin.site.register(Profile)
 admin.site.register(Transactions)
+admin.site.register(Subscriptions)
+admin.site.register(Budget)
