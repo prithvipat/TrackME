62bf5289e6a01dd6ce7a677abf4636ce75c5e933
Prithvi Paturi <87906760+prithvipat@users.noreply.github.com>
2024-09-17 11:25:19 -0400

    Add files via upload

---
+++ TrackME/user/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Subscriptions, Transactions, Profile
+
+# Register your models here.
+admin.site.register(Profile)
+admin.site.register(Transactions)
+admin.site.register(Subscriptions)
