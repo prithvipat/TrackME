719c3e1aa3ecc96458ce1eaedd07a38381259564
Prithvi Paturi <87906760+prithvipat@users.noreply.github.com>
2024-12-10 14:51:36 -0500

    Update urls.py

--- TrackME/user/urls.py
+++ TrackME/user/urls.py
@@ -2,11 +2,10 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.check_transactions, name='index'),
     path('signup/', views.signup, name='signup'),
     path('login/', views.login, name='login'),
     path('logout/', views.logout, name='logout'),
-    path('profile/', views.check_transactions, name='profile'),
     path('settings/', views.settings, name='settings'),
     path('delete_transactions/<event_id>',views.delete_transactions, name='delete_transactions'),
     path('delete_subscriptions/<event_id>', views.delete_subscriptions, name='delete_subscriptions'),
